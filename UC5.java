b4576bad7a68eaa336b0ee6badf42ab08b938944
srinidhieadara-07 <srinidhieadara@gmail.com>
2026-03-10 13:10:53 +0530

    Initial project setup

---
+++ UC5.java
@@ -0,0 +1,21 @@
+public class UC5 {
+    
+}
+public class PrintOOPS {
+    public static void main(String[] args) {
+
+        String[] oops = {
+            " *****    *****    ******    *****  ",
+            "*     *  *     *   *     *   *     * ",
+            "*     *  *     *   *     *   *       ",
+            "*     *  *     *   *****     *****  ",
+            "*     *  *     *   *              * ",
+            "*     *  *     *   *        *     * ",
+            " *****    *****    *         *****  "
+        };
+
+        for (String line : oops) {
+            System.out.println(line);
+        }
+    }
+}
